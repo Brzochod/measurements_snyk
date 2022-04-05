bb280bc3b3140fb73f60dd7b5e0e5f415db981b1
Radim Vaňko <radim.vanko@3pillarglobal.com>
2022-04-05 09:48:35 +0200

    projects setup
    Co-authored-by: brzochod-3pillarglobal <brzochod-3pillarglobal@users.noreply.github.com>

---
+++ apps/meas-sve/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'meas-sve',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^(.+\\.svelte$)': [
+      'svelte-jester',
+      {
+        preprocess: 'apps/meas-sve/svelte.config.cjs',
+      },
+    ],
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['svelte', 'ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/meas-sve',
+};
